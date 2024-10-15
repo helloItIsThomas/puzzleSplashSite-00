bb4a2431a86fa1ae17d7b8e65d0bd21ae9b9697d
Thomas McElmeel <helloitisthomasm@gmail.com>
2024-10-15 16:15:42 -0400

    init

---
+++ scripts/emailForm.js
@@ -0,0 +1,40 @@
+import emailJs from "@emailjs/browser";
+
+document.getElementById("myForm").addEventListener("submit", (e) => {
+  e.preventDefault();
+  handleSubmit(e);
+});
+
+function handleSubmit(e) {
+  const form = {
+    FirstName: e.target.fName.value,
+    LastName: e.target.lName.value,
+    Email: e.target.email.value,
+    Message: e.target.message.value,
+  };
+  console.log(form);
+
+  const serviceID = "string";
+  const templateID = "string";
+  const publicKey = "String";
+
+  emailJs
+    .send(serviceID, templateID, form, publicKey)
+    .then((res) => {
+      return res;
+    })
+    .then((data) => {
+      console.log(data);
+      if (data.status < 299) {
+        console.log("SUCCESS");
+
+        e.target.fName.value = "";
+        e.target.lName.value = "";
+        e.target.email.value = "";
+        e.target.message.value = "";
+      }
+    })
+    .catch((error) => {
+      console.log("ERROR: " + JSON.stringify(error));
+    });
+}
