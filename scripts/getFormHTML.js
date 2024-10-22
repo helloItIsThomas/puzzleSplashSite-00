a463df539048bb3973309b0daa4d05ed5684802d
Thomas McElmeel <helloitisthomasm@gmail.com>
2024-10-22 16:12:14 -0400

    fixing

--- scripts/getFormHTML.js
+++ scripts/getFormHTML.js
@@ -1,17 +1,7 @@
 import { listenToForm } from "./emailForm.js";
 import { prepareTimeline } from "/scripts/formAnims";
 
-fetch("/html/form.html")
-  .then((response) => response.text())
-  .then((data) => {
-    let tempDiv = document.createElement("div");
-    tempDiv.innerHTML = data;
-    let divToMove = tempDiv.querySelector("#mailForm");
-    document.getElementById("myForm").appendChild(divToMove);
-  })
-  .then((data) => {
-    listenToForm();
-    prepareTimeline();
-    const form = document.querySelector("#mailForm");
-    form.scrollTo(0, 0);
-  });
+listenToForm();
+prepareTimeline();
+const form = document.querySelector("#mailForm");
+form.scrollTo(0, 0);
