7e522e003f7acefe8d2c2e7be790539145c7a8f8
Thomas McElmeel <helloitisthomasm@gmail.com>
2024-10-22 16:16:54 -0400

    fixing button getting too small issue

--- scripts/emailForm.js
+++ scripts/emailForm.js
@@ -8,29 +8,14 @@ export function listenToForm() {
     handleSubmit(e);
   });
 
-  // document
-  // .getElementById("formFields")
-  // .addEventListener("submit", function (event) {
-  // event.preventDefault();
-  // console.log("SUBMITTED");
-  // closeAnim();
-  // });
-
   document.getElementById("closeButton").addEventListener("click", () => {
     formToggle();
-    // if (formState.isClosed) {
-    // openAnim();
-    // } else if (!formState.isClosed) {
-    // closeAnim();
-    // }
   });
 }
 
 export function checkHandleSubmit() {
   document.getElementById("mailForm").addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log("§ SUBMITTED FORM §");
-    // handleSubmit(e);
   });
 }
 
@@ -47,22 +32,31 @@ function handleSubmit(e) {
   const templateID = "template_96et0s4";
   const publicKey = "HGRBfY6kZJDUteYUu";
 
-  emailJs
-    .send(serviceID, templateID, form, publicKey)
-    .then((res) => {
-      return res;
-    })
-    .then((data) => {
-      console.log(data);
-      if (data.status < 299) {
-        console.log("SUCCESS");
+  const sendButton = document.getElementById("sendButton");
+  sendButton.innerText = "Sent!";
+  setTimeout(() => {
+    formToggle();
+    setTimeout(() => {
+      sendButton.innerText = "Send";
+    }, 2000);
+  }, 1000);
 
-        e.target.name.value = "";
-        e.target.email.value = "";
-        e.target.message.value = "";
-      }
-    })
-    .catch((error) => {
-      console.log("ERROR: " + JSON.stringify(error));
-    });
+  // emailJs
+  // .send(serviceID, templateID, form, publicKey)
+  // .then((res) => {
+  // return res;
+  // })
+  // .then((data) => {
+  // console.log(data);
+  // if (data.status < 299) {
+  // console.log("SUCCESS");
+  //
+  // e.target.name.value = "";
+  // e.target.email.value = "";
+  // e.target.message.value = "";
+  // }
+  // })
+  // .catch((error) => {
+  // console.log("ERROR: " + JSON.stringify(error));
+  // });
 }
